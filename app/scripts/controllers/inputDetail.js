c4ae23132dd80ab6888de6a9605434d34e899ac9
Adam L <github@adamin.net>
2013-05-27 21:04:15 -0700

    Added data req for input detail

--- app/scripts/controllers/inputDetail.js
+++ app/scripts/controllers/inputDetail.js
@@ -2,5 +2,11 @@
 
 angular.module('formInputHelperApp')
   .controller('InputDetailCtrl', function ($scope, inputService, $routeParams) {
+
     $scope.inputType = $routeParams.inputType;
+
+    inputService.getInputs().then(function(d) {
+        $scope.inputs = d.inputTypes;
+    });
+
   });
