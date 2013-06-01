70ab1947d81d8a64712c4e741f7249c03216d63f
Adam L <github@adamin.net>
2013-06-01 14:08:18 -0700

    Added datalist directive

---
+++ app/scripts/directives/datalist.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('formInputHelperApp')
+  .directive('datalist', function () {
+    return {
+      templateUrl: '/views/partials/datalist.html',
+      restrict: 'A',
+      replace: true,
+      link: function postLink(scope, element, attrs) {
+      }
+    };
+  });
