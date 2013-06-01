408ebdeb75766187a83c8c5c9a5939b7da82b48a
Adam L <github@adamin.net>
2013-06-01 15:19:08 -0700

    Refactored inputDetail controller

--- app/scripts/controllers/inputDetail.js
+++ app/scripts/controllers/inputDetail.js
@@ -1,29 +1,28 @@
 'use strict';
 
 angular.module('formInputHelperApp')
-  .controller('InputDetailCtrl', function ($scope, inputService, $routeParams) {
+  .controller('InputDetailCtrl', function($scope, inputService, $routeParams) {
 
-    $scope.inputType = $routeParams.inputType;
+  $scope.inputType = $routeParams.inputType;
 
-    inputService.getInputs().then(function(d) {
-        var inputsList = d.inputTypes;
-        // iterate through input types based on $routeParam
-        var findInput = function(val) {
-            for (var i = inputsList.length - 1; i >= 0; i--) {
-                if (inputsList[i].type === val) {
-                    return inputsList[i];
-                    break;
-                };
-            }
-        };
-        // assign $scope.input
-        $scope.input = findInput($scope.inputType);
-        return
-    });
-
-    // // getting variable out of $scope for faster loop
-    // var inputsList = $scope.inputs;
-    // var inputType = $scope.inputType;
-
-
+  inputService.getInputs().then(function(d) {
+    var inputsList = d.inputTypes;
+    // iterate through input types based on $routeParam
+    var findInput = function(val) {
+      for (var i = inputsList.length - 1; i >= 0; i--) {
+        if (inputsList[i].type === val) {
+          return inputsList[i];
+        }
+      }
+    };
+    // assign $scope.input
+    $scope.input = findInput($scope.inputType);
+    return;
   });
+
+  // // getting variable out of $scope for faster loop
+  // var inputsList = $scope.inputs;
+  // var inputType = $scope.inputType;
+
+
+});
