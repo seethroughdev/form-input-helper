138ba8e1ffdc218cbb2303a3e588545596ebc588
Adam L <github@adamin.net>
2013-06-06 09:43:06 -0700

    Temporary layout improvement

--- app/scripts/controllers/inputDetail.js
+++ app/scripts/controllers/inputDetail.js
@@ -10,23 +10,30 @@ angular.module('formInputHelperApp')
   inputService.getInputs().then(function(d) {
     var inputsList = d.inputTypes;
     // iterate through input types based on $routeParam
-    var findInput = function(val) {
+    var findInput = function(val, offset) {
+      if (!offset) {
+        offset = 0;
+      }
       for (var i = inputsList.length - 1; i >= 0; i--) {
         if (inputsList[i].type === val) {
-          return inputsList[i];
+          return inputsList[i+offset];
         }
       }
     };
     // assign $scope.input
-    $scope.input = findInput(inputType);
+    var input = $scope.input = findInput(inputType);
+    $scope.nextInputType = findInput(inputType, 1);
+    $scope.prevInputType = findInput(inputType, -1);
+
 
     // $scope.placeholder = $scope.input.attr.placeholder;
 
     // $scope.input.attr.pattern = false;
 
-    $scope.selectorType = 'input[type="' + $scope.input.type + '"]';
+    $scope.selectorType = 'input[type="' + inputType + '"]';
 
     $scope.slide = 'desc';
+
 
     return;
   });
